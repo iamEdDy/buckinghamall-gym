989bb866511fdf499059753e8edc16214bfe2b6b
EdDy <54478576+iamEdDy@users.noreply.github.com>
2024-12-29 23:22:34 +0100

    Update monthly-plan.js

--- js/monthly-plan.js
+++ js/monthly-plan.js
@@ -1,7 +1,7 @@
   let monthlyAmount;
   function setAmountMonthlyGym() {
-      monthlyAmount = 22000;
-      document.getElementById('gym-monthly-amount').value = 22000;
+      monthlyAmount = 25000;
+      document.getElementById('gym-monthly-amount').value = 25000;
   }
   
   
